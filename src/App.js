37d5e8220bb16ed9df9870261d36bca01455cdfc
Tony Fite <koopastrong@gmail.com>
2019-03-06 15:15:24 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/ico.png';
 import './App.css';
+import './fonts/slkscr.ttf'
+import {tiles} from './data/index'
+import './features/logo/index.css';
+import Player from './features/player/player';
+
+
+let  player = Player;
+
+function getTile(type) {
+  switch(type){
+    case 0:  return 'default'
+    case 1:  return 'hair'
+    case 2:  return 'skin'
+    case 3:  return 'clothes'
+    case 4:  return 'skinShade'
+    case 5:  return 'boots'
+    case 6:  return 'bootsShade'
+    case 7:  return 'clotheShade'
+    case 8: return 'bootsLight'
+    case 9: return 'black'
+  }
+}
+
+function MapTile(player) {
+  return <div className = {`tile ${getTile(player.tile)}`}>{player.tile}</div>
+}
+
+function MapRow(player) {
+  return <div className="row">
+    {
+      player.tiles.map(tile => <MapTile tile={tile} />)
+    }
+  </div>
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <p className="title">8-Bit</p>
+       <div className = 'bh'>
+          <img src=  {
+              player.tiles.map(row => <MapRow tiles={row} />)
+            }
+             className="App-logo" alt="logo" />
+        </div>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+             Game
           </p>
           <a
             className="App-link"
---
+++ src/features/player/player.js
@@ -0,0 +1,14 @@
+import {React, Component} from 'react';
+import tiles from '../../data/index';
+
+class Player extends Component{
+  render() {
+    return (
+      <div className="player">
+
+      </div>
+    );
+  }
+}
+
+export default Player;
