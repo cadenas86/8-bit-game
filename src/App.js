817325da9d0f61d17198467efd57f27eda05e12d
Tony Fite <koopastrong@gmail.com>
2019-03-06 22:15:18 -0500

    changed up some code!

--- src/App.js
+++ src/App.js
@@ -2,46 +2,8 @@ import React, { Component } from 'react';
 import logo from './images/ico.png';
 import './App.css';
 import './fonts/slkscr.ttf'
-import {tiles} from './data/index'
-import './features/logo/index.css';
+import {tiles, drawPixels} from './data/index'
 import Player from './features/player/player';
-
-/*This code is getting the Player class from features/player/player.js which is
- linked to data/index.js and that's where the 16X16 pixel array lies which consists
- of numbers ranging between 0 and 9 to display different colors of each pixel in
- that 16X16 square. That's basically how this game will work.*/
-
-let  player = Player;
-
-/*This gets the tile numbers from data/index.js and displays each one as a
-className such as default, or hair. default is transparent.*/
-
-function getTile(type) {
-  switch(type){
-    case 0:  return 'default'
-    case 1:  return 'hair'
-    case 2:  return 'skin'
-    case 3:  return 'clothes'
-    case 4:  return 'skinShade'
-    case 5:  return 'boots'
-    case 6:  return 'bootsShade'
-    case 7:  return 'clotheShade'
-    case 8: return 'bootsLight'
-    case 9: return 'black'
-  }
-}
-
-function MapTile(player) {
-  return <div className = {`tile ${getTile(player.tile)}`}>{player.tile}</div>
-}
-
-function MapRow(player) {
-  return <div className="row">
-    {
-      player.tiles.map(tile => <MapTile tile={tile} />)
-    }
-  </div>
-}
 
 class App extends Component {
   render() {
@@ -50,10 +12,7 @@ class App extends Component {
         <header className="App-header">
         <p className="title">8-Bit</p>
        <div className = 'bh'>
-          <img src=  {
-              player.tiles.map(row => <MapRow tiles={row} />)
-            }
-             className="App-logo" alt="logo" />
+          <drawPixels className="App-logo" />
         </div>
           <p>
              Game
--- src/data/index.js
+++ src/data/index.js
@@ -1,21 +1,58 @@
 //Each number represents a pixel and in this case the pixel is 16X16 as an image//
+import React from 'react';
 
 export const tiles = [
-   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 1, 4, 1, 4, 1, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 1, 4, 4, 4, 1, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 2, 2, 3, 7, 4, 7, 3, 2, 2, 0, 0, 0, 0],
-   [0, 0, 0, 2, 2, 3, 3, 9, 3, 3, 2, 2, 0, 0, 0, 0],
-   [0, 0, 0, 0, 7, 7, 3, 3, 3, 7, 7, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 9, 3, 9, 3, 9, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 6, 8, 3, 3, 3, 8, 6, 0, 0, 0, 0, 0],
-   [0, 0, 0, 6, 5, 8, 6, 0, 6, 8, 5, 6, 0, 0, 0, 0],
-   [0, 0, 0, 6, 5, 5, 6, 0, 6, 5, 5, 6, 0, 0, 0, 0],
-   [0, 0, 0, 0, 6, 6, 0, 0, 0, 6, 6, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 1, 4, 1, 4, 1, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 1, 4, 4, 4, 1, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 2, 2, 3, 7, 4, 7, 3, 2, 2, 0, 0, 0, 0,
+   0, 0, 0, 2, 2, 3, 3, 9, 3, 3, 2, 2, 0, 0, 0, 0,
+   0, 0, 0, 0, 7, 7, 3, 3, 3, 7, 7, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 9, 3, 9, 3, 9, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 6, 8, 3, 3, 3, 8, 6, 0, 0, 0, 0, 0,
+   0, 0, 0, 6, 5, 8, 6, 0, 6, 8, 5, 6, 0, 0, 0, 0,
+   0, 0, 0, 6, 5, 5, 6, 0, 6, 5, 5, 6, 0, 0, 0, 0,
+   0, 0, 0, 0, 6, 6, 0, 0, 0, 6, 6, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ]
+
+/*This code is getting the Player class from features/player/player.js which is
+ linked to data/index.js and that's where the 16X16 pixel array lies which consists
+ of numbers ranging between 0 and 9 to display different colors of each pixel in
+ that 16X16 square. That's basically how this game will work.*/
+
+let pixelsDrawn = false;
+
+/*This gets the tile numbers from data/index.js and displays each one as a
+className such as default, or hair. default is transparent.*/
+
+function getTile(type) {
+  switch(type){
+    case 0:  return 'default'
+    case 1:  return 'hair'
+    case 2:  return 'skin'
+    case 3:  return 'clothes'
+    case 4:  return 'skinShade'
+    case 5:  return 'boots'
+    case 6:  return 'bootsShade'
+    case 7:  return 'clotheShade'
+    case 8: return 'bootsLight'
+    case 9: return 'black'
+  }
+}
+
+function MapTile(tile) {
+  return <div className = {`tile ${getTile(tile)}`}>{tile}</div>
+}
+
+export function drawPixels() {
+  let tile;
+  for(var pixel = 0; pixel<tiles.length; pixel++) {
+    tile = tiles[pixel];
+  }
+  return <MapTile tile={tile} />
+}
